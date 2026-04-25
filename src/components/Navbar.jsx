149f323519f16d3aee69be1d11893aa6ecdd9504
kalincreative <kalincreativee@gmail.com>
2026-04-26 01:26:58 +0800

    fix: remove logo icon from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,6 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center gap-2">
-            <img src="/favicon/favicon-32x32.png" alt="KaFlix Space" className="h-8 w-auto" />
             <span className="text-xl font-semibold"><span className="text-[#FF1493]">KaFlix</span> Space</span>
           </Link>
 
